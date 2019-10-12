cd5938deee9d87088f653b7cfe4369a3b14f0f63
dengchong.999 <dengchong.999@bytedance.com>
2019-10-12 17:13:58 +0800

    添加线程安全队列

---
+++ core/src/main/cpp/common/ThreadSafeQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dengchong on 2019-10-12.
+//
+
+#include "ThreadSafeQueue.h"
---
+++ core/src/main/cpp/common/ThreadSafeQueue.h
@@ -0,0 +1,79 @@
+//
+// Created by dengchong on 2019-10-12.
+//
+
+#ifndef AUVIDEO_THREADSAFEQUEUE_H
+#define AUVIDEO_THREADSAFEQUEUE_H
+
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+template<typename T>
+class ThreadSafeQueue {
+private:
+    std::mutex mutex;
+    std::condition_variable cond;
+    std::queue<T> data;
+public:
+    ThreadSafeQueue() {
+
+    }
+
+    ThreadSafeQueue(const ThreadSafeQueue &other) {
+        std::lock_guard<std::mutex> lockGuard(mutex);
+        data = other.data;
+    }
+
+    ThreadSafeQueue &operator=(const ThreadSafeQueue &other) = delete;
+
+    void push(T value) {
+        std::lock_guard<std::mutex> lockGuard(mutex);
+        data.push(value);
+        cond.notify_one();
+    }
+
+    void waitAndPop(T &value) {
+        std::unique_lock<std::mutex> uniqueLock(mutex);
+        cond.wait(uniqueLock, [this] { return !data.empty(); });
+        value = data.front();
+        data.pop();
+    }
+
+    std::shared_ptr<T> waitAndPop() {
+        std::unique_lock<std::mutex> uniqueLock(mutex);
+        cond.wait(uniqueLock, [this] { return !data.empty(); });
+        std::shared_ptr<T> res(std::make_shared<T>(data.front()));
+        data.pop();
+        return res;
+    }
+
+    bool tryPop(T &value) {
+        std::lock_guard<std::mutex> lockGuard(mutex);
+        if (data.empty()) {
+            return false;
+        }
+        value = data.front();
+        data.pop();
+        return true;
+    }
+
+    std::shared_ptr<T> tryPop() {
+        std::lock_guard<std::mutex> lockGuard(mutex);
+        if (data.empty()) {
+            return nullptr;
+        }
+        std::shared_ptr<T> res(std::make_shared<T>(data.front()));
+        data.pop();
+        return res;
+    }
+
+    bool empty() const {
+        std::lock_guard lockGuard(mutex);
+        return data.empty();
+    }
+};
+
+
+#endif //AUVIDEO_THREADSAFEQUEUE_H
