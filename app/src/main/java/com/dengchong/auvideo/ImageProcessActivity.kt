c20f23bf9fd9dc99a1b01430653440efcc738e4d
dengchong.999 <dengchong.999@bytedance.com>
2020-01-06 21:55:39 +0800

    feat(av): 添加 ffmpeg_core 工作台

---
+++ app/src/main/java/com/dengchong/auvideo/ImageProcessActivity.kt
@@ -0,0 +1,11 @@
+package com.dengchong.auvideo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageProcessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_image_process)
+    }
+}
---
+++ core/src/main/cpp/ffmpeg_core/FFmpegUtil.h
@@ -0,0 +1,14 @@
+//
+// Created by dengchong on 2020-01-06.
+//
+
+#ifndef AUVIDEO_FFMPEGUTIL_H
+#define AUVIDEO_FFMPEGUTIL_H
+
+
+class FFmpegUtil {
+
+};
+
+
+#endif //AUVIDEO_FFMPEGUTIL_H
